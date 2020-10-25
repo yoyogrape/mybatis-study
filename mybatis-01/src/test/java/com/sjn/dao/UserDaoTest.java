beed83cf923ea921f24fc6403a98d37eaa503d26
jasmine <360347327@qq.com>
2020-10-25 21:19:26 +0800

    第一个完整可运行版本，支持查询

--- mybatis-01/src/test/java/com/sjn/dao/UserDaoTest.java
+++ mybatis-01/src/test/java/com/sjn/dao/UserDaoTest.java
@@ -10,11 +10,12 @@ import java.util.List;
 public class UserDaoTest {
     @Test
     public void findAll() {
-        SqlSession sqlSession = MybatisUtil.getSqlSession();
-        UserDao dao = sqlSession.getMapper(UserDao.class);
-        List<User> userList = dao.findAll();
-        for (User user : userList) {
-            System.out.println(user);
+        try (SqlSession sqlSession = MybatisUtil.getSqlSession()) {
+            UserDao dao = sqlSession.getMapper(UserDao.class);
+            List<User> userList = dao.findAll();
+            for (User user : userList) {
+                System.out.println(user);
+            }
         }
     }
 }

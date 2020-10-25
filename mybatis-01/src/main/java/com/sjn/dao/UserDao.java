a4aa7f2f21266cebc3bd5f30d2c8e9b29eb89981
jasmine <360347327@qq.com>
2020-10-25 19:54:54 +0800

    初始环境搭建完成

---
+++ mybatis-01/src/main/java/com/sjn/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.sjn.dao;
+
+import com.sjn.opjo.User;
+
+import java.util.List;
+
+public interface UserDao {
+    List<User> findAll();
+}
---
+++ mybatis-01/src/main/java/com/sjn/utils/MybatisUtil.java
@@ -0,0 +1,31 @@
+package com.sjn.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MybatisUtil {
+    //获取工厂
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        try {
+            String resource = "mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("..err..获取工厂失败...");
+        }
+    }
+
+    public static SqlSession getSqlSession() {
+        return sqlSessionFactory.openSession();
+    }
+
+
+}
